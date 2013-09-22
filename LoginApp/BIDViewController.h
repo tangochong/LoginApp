fc423bdc9925d739e8989345c959a4ccb59cbeb0
keenray <keenray@keenraytekiMac-mini.local>
2013-09-22 16:42:47 +0800

    Initial Commit

---
+++ LoginApp/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  LoginApp
+//
+//  Created by keenray on 13-9-22.
+//  Copyright (c) 2013年 keenray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ LoginApp/BIDViewController.m
@@ -0,0 +1,29 @@
+//
+//  BIDViewController.m
+//  LoginApp
+//
+//  Created by keenray on 13-9-22.
+//  Copyright (c) 2013年 keenray. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
