f6112609621b0678b75aabb7dff662d690ad2a36
keenray <keenray@keenraytekiMac-mini.local>
2013-09-30 10:23:29 +0800

    dd

--- SinaViewController.h
+++ SinaViewController.h
@@ -7,6 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#define OAuth_URL @"https://api.weibo.com/oauth2/authorize"
+#define APP_KEY @"318745286"
+#define APP_REDIRECT_URL @"http://weibo.com/638298000/home"
 
 @interface SinaViewController : UIViewController<UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
